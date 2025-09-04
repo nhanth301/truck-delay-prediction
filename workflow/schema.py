fcb38bc197becd075bc19ffb753506e7f182eabc
nhanth301 <nhanthien.tnn@gmail.com>
2025-09-04 11:34:57 +0700

    add new key

--- workflow/schema.py
+++ workflow/schema.py
@@ -6,4 +6,5 @@ class State(TypedDict, total=False):
     config: Dict[str, Any]
     should_continue: bool
     new_data: Dict[str, pd.DataFrame]
+    new_data_status: Dict[str, bool]
     new_data_quality: Dict[str, bool]   
