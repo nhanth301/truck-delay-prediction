168a8602af893660a18a19bbddb21f0de6b4b6e6
nhanth301 <nhanthien.tnn@gmail.com>
2025-09-04 10:58:38 +0700

    up data type ano

---
+++ workflow/schema.py
@@ -0,0 +1,9 @@
+from langgraph.graph import StateGraph
+from typing import TypedDict, List, Annotated, Dict, Any
+import pandas as pd
+
+class State(TypedDict, total=False):
+    config: Dict[str, Any]
+    should_continue: bool
+    new_data: Dict[str, pd.DataFrame]
+    new_data_quality: Dict[str, bool]
