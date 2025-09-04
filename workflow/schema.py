b198a07f04e11ac5fff58ca44ddcca4cba7320d7
nhanth301 <nhanthien.tnn@gmail.com>
2025-09-04 11:09:34 +0700

    cm

--- workflow/schema.py
+++ workflow/schema.py
@@ -6,4 +6,4 @@ class State(TypedDict, total=False):
     config: Dict[str, Any]
     should_continue: bool
     new_data: Dict[str, pd.DataFrame]
-    new_data_quality: Dict[str, bool]
+    new_data_quality: Dict[str, bool]   
