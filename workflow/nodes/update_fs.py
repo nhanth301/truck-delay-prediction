bd68d4a61d331c4b8763154c2000854fc89c6d36
nhanth301 <nhanthien.tnn@gmail.com>
2025-09-04 12:15:52 +0700

    rebuild final

--- workflow/nodes/update_fs.py
+++ workflow/nodes/update_fs.py
@@ -1,18 +1,34 @@
 from workflow.schema import State
 import hopsworks
-
+from pipeline.utils import fetch_data
+from pipeline.data_prep import processing_and_feature_engg
 def init_fs(config):
     project = hopsworks.login(api_key_value=config['hopsworks']['api_key'])
     fs = project.get_feature_store()
     return fs 
 
-def update_fs(config, fs, feature_group_name, new_data):
+def update_fs(config, fs, feature_group_name, new_data, overwrite=False):
     feature_group = fs.get_feature_group(
         feature_group_name,
         version=config['hopsworks']['feature_group_version']
     )
-    feature_group.insert(new_data)
+    feature_group.insert(new_data,overwrite=overwrite)
     print(f"✅ Inserted {len(new_data)} rows into {feature_group_name}")
+
+def rebuild_merged_feature_group(config, fs):
+    routes_df = fetch_data(config, 'routes_details_fg')
+    trucks_df = fetch_data(config, 'truck_details_fg')
+    drivers_df = fetch_data(config, 'drivers_details_fg')
+    truck_schedule_df = fetch_data(config, 'truck_schedule_details_fg')
+    traffic_df = fetch_data(config, 'traffic_details_fg')
+    city_weather_df = fetch_data(config, 'city_weather_details_fg')
+    route_weather_df = fetch_data(config, 'route_weather_details_fg')
+    fs_data = processing_and_feature_engg(routes_df,route_weather_df,drivers_df,trucks_df,traffic_df,truck_schedule_df,city_weather_df,0)
+    print(f"🔄 Rebuilding merged feature group with {len(fs_data)} rows...")
+    if fs_data.empty:
+        print("⚠️ No data to insert into final_data")
+    else:
+        update_fs(config, fs, 'final_data', fs_data, overwrite=True)
 
 def update_feature_store(state: State):
     fs = init_fs(state['config'])
@@ -23,6 +39,15 @@ def update_feature_store(state: State):
         "city_weather":"city_weather_details_fg"
     }
 
+    update_status = {}
     for key in key_to_fg:
         if state['new_data_quality'][key] and state['new_data_status'][key]:
             update_fs(state['config'], fs, key_to_fg[key], state["new_data"][key])
+            update_status[key] = f"Inserted {len(state['new_data'][key])} rows"
+        else:
+            update_status[key] = "Skipped (quality fail or no data)"
+
+    rebuild_merged_feature_group(state['config'], fs)
+    update_status['final_data'] = "Rebuilt"
+
+    return {"update_status": update_status}
